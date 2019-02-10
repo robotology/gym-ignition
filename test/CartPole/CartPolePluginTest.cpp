b46226f90a7fc7be8b4724c787a3fd0897fecbb4
Diego Ferigo <diego.ferigo@iit.it>
2019-02-10 13:00:09 +0100

    First working simulation of CartPolePluginTest

--- test/CartPole/CartPolePluginTest.cpp
+++ test/CartPole/CartPolePluginTest.cpp
@@ -1,47 +1,33 @@
-#include <iostream>
-
-//#include "CartPole.h"
 #include "gympp/Gympp.h"
 #include "gympp/Log.h"
 #include "gympp/common.h"
 #include "gympp/gyms/Ignition.h"
+#include "gympp/spaces/Space.h"
 
-#include <chrono>
-#include <thread>
-#include <typeindex>
-#include <typeinfo>
-#include <variant>
+#include <iostream>
 
 class Gym
 {
 public:
-    //    struct CartPole
-    //    {
-    //        using ObservationBuffer = gympp::BufferDouble;
-    //    };
-
-    static gympp::Environment* make(const std::string& envName)
+    static gympp::EnvironmentPtr make(const std::string& envName)
     {
         if (envName == "CartPole") {
+            // TODO: find file in the fs
             std::string sdfFile =
                 "/home/dferigo/git/gym-ignition/models/CartPole/CartPoleWorld.sdf";
 
             using OSpace = gympp::spaces::Box;
             using ASpace = gympp::spaces::Discrete;
 
-            using ActionDataType = size_t;
-            using ObservationDataType = double;
-
-            auto* ignGym = new gympp::gyms::IgnitionGazebo<ActionDataType, ObservationDataType>(
+            auto ignGym = std::make_shared<gympp::gyms::IgnitionGazebo>(
                 "libCartPolePlugin.so",
                 "gympp::plugins::CartPole",
-                std::make_shared<ASpace>(2),
-                std::make_shared<OSpace>(OSpace::Limit{-90, -20}, OSpace::Limit{90, 20}),
+                std::make_shared<ASpace>(3),
+                std::make_shared<OSpace>(OSpace::Limit{-90, -1}, OSpace::Limit{90, 1}),
                 sdfFile,
-                /*updateRate=*/10,
-                /*iterations=*/100);
+                /*updateRate=*/1000,
+                /*iterations=*/1);
 
-            // TODO: how to handle the memory deallocation?
             return ignGym->env();
         }
 
@@ -60,27 +46,28 @@ int main(int /*argc*/, char* /*argv*/[])
 
     Environment::State oldState;
 
+    // TODO: command line option for setting the render mode
     if (!env->render(Environment::RenderMode::HUMAN)) {
         gymppError << "Failed to render the environment" << std::endl;
         return EXIT_FAILURE;
     }
 
-    //    std::chrono::milliseconds period{100};
-    //    std::cout << "Period " << period.count() << "ms" << std::endl;
+    // Initialize the seed
+    // TODO: command line option for reproducible simulation
+    env->seed();
+    // env->seed(42);
 
     size_t epoch = 0;
+    size_t iteration = 1;
 
     while (true) {
-        std::cout << "#" << epoch++ << " " << std::flush;
+        std::cout << "#" << epoch << "." << iteration++ << " " << std::flush;
 
-        //        auto tick = std::chrono::high_resolution_clock::now();
-
-        // TODO: use oldState to obtain the action
-
-        // Here we use a random action to bypass it
-        //        auto action = Environment::Action(std::valarray<size_t>{1});
-        //        auto* action = actionSample.get<ActionDataType>();
+        // Process oldState to obtain the action.
+        // Here we use a random action to bypass it.
         auto actionSample = env->action_space->sample();
+
+        // Simulate the system with the given action
         auto state = env->step(actionSample);
 
         if (!state.has_value()) {
@@ -88,16 +75,12 @@ int main(int /*argc*/, char* /*argv*/[])
             return EXIT_FAILURE;
         }
 
-        if (state->done) {
-            gymppDebug << "The environment reached the terminal state" << std::endl;
-            break;
-        }
-
+        // Print the observation
         if (auto* o = state->observation.get<double>(); o) {
             for (const auto el : *o) {
                 std::cout << el << " ";
             }
-            std::cout << std::endl;
+            std::cout << std::endl << std::flush;
         }
         else {
             gymppError << "The environment didn't return the observation" << std::endl;
@@ -110,22 +93,26 @@ int main(int /*argc*/, char* /*argv*/[])
         // Save the old state
         oldState = std::move(state.value());
 
-        //        auto tock = std::chrono::high_resolution_clock::now();
-        //        auto computed = std::chrono::duration_cast<std::chrono::milliseconds>(tock -
-        //        tick); auto sleep = period - computed;
-
-        //        std::cout << "[Loop " << computed.count() << "ms]";
-
-        //        if (sleep.count() > 0) {
-        //            //            std::cout << sleep.count() << std::endl;
-        //            std::this_thread::sleep_for(sleep);
-        //        }
-
-        std::cout << std::endl;
+        // Handle termination
+        if (state->done) {
+            gymppDebug << "The environment reached the terminal state" << std::endl;
+            //            break;
+            auto newObservation = env->reset();
+            if (!newObservation) {
+                gymppError << "Failed to reset the environment" << std::endl;
+                return EXIT_FAILURE;
+            }
+            else {
+                epoch++;
+                iteration = 0;
+                oldState.observation = newObservation.value();
+                oldState.reward = 0;
+                oldState.done = false;
+                gymppMessage << "Resetting the environment" << std::endl;
+                continue;
+            }
+        }
     }
 
     return EXIT_SUCCESS;
-
-    //    auto sample = gympp::data::Sample{action};
-    //    auto state = env->step(sample);
 }
