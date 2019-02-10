ad480fb9a201f7cb3483a5c8c618d3fb40adba10
Diego Ferigo <diego.ferigo@iit.it>
2019-02-10 12:59:05 +0100

    First working version of CartPolePlugin
    It requires a patched version of ign-gazebo

--- envs/cpp/CartPole/CartPolePlugin.cpp
+++ envs/cpp/CartPole/CartPolePlugin.cpp
@@ -1,144 +1,256 @@
 #include "CartPolePlugin.h"
+#include "gympp/Random.h"
 
 #include <ignition/gazebo/Model.hh>
 #include <ignition/plugin/Register.hh>
 
 #include <ignition/gazebo/components/Joint.hh>
+#include <ignition/gazebo/components/JointPosition.hh>
 #include <ignition/gazebo/components/Link.hh>
 #include <ignition/gazebo/components/Model.hh>
 #include <ignition/gazebo/components/Name.hh>
 #include <ignition/gazebo/components/Pose.hh>
 
+#include <cmath>
 #include <memory>
 
 using namespace gympp::plugins;
+using namespace ignition::gazebo::components;
+
+enum CartPoleAction
+{
+    MOVE_RIGHT,
+    MOVE_LEFT,
+    DONT_MOVE,
+};
 
 class CartPole::Impl
 {
 public:
+    unsigned seed;
     bool firstRun = true;
     mutable std::mutex mutex;
     ignition::gazebo::Model model;
+
+    CartPoleAction action;
+    BufferDouble observationBuffer;
+
+    ignition::gazebo::EntityComponentManager* manager = nullptr;
+
+    double getRandomTheta();
 };
+
+double CartPole::Impl::getRandomTheta()
+{
+    std::uniform_real_distribution<> distr(-25.0 * (M_PI / 180.0), 25.0 * (M_PI / 180.0));
+    return distr(gympp::Random::engine());
+}
 
 CartPole::CartPole()
     : pImpl{new Impl(), [](Impl* impl) { delete impl; }}
-{}
+{
+    // - Pole angular position [deg]
+    // - Cart linear position [m]
+    pImpl->observationBuffer.resize(2);
+}
 
 // TODO: this is not called if the plugin is not put in the sdf.
 //       Probably this action will be supported in the future.
-void CartPole::Configure(const ignition::gazebo::EntityId& _id,
+void CartPole::Configure(const ignition::gazebo::Entity& /*entity*/,
                          const std::shared_ptr<const sdf::Element>& /*_sdf*/,
-                         ignition::gazebo::EntityComponentManager& _ecm,
+                         ignition::gazebo::EntityComponentManager& manager,
                          ignition::gazebo::EventManager& /*_eventMgr*/)
 {
-    igndbg << "Configuring!" << std::endl;
-    this->pImpl->model = ignition::gazebo::Model(_id);
+    // Save the manager. It is used in reset().
+    pImpl->manager = &manager;
 
-    auto link = this->pImpl->model.LinkByName(_ecm, "pole");
+    // Get the model from the entity manager
+    auto id = manager.EntityByComponents(ignition::gazebo::components::Model(),
+                                         ignition::gazebo::components::Name("cartpole_xacro"));
+    this->pImpl->model = ignition::gazebo::Model(id);
 
-    // Fail to create component if link is not found
-    if (link == ignition::gazebo::kNullEntity) {
-        ignerr << "Failed to find link" << std::endl;
+    // Get the links from the model
+    auto pole = this->pImpl->model.LinkByName(manager, "cartpole_xacro::pole");
+    auto cart = this->pImpl->model.LinkByName(manager, "cartpole_xacro::cart");
+    auto rail = this->pImpl->model.LinkByName(manager, "cartpole_xacro::rail");
 
+    // Get the joints
+    auto pivot = this->pImpl->model.JointByName(manager, "cartpole_xacro::pivot");
+    auto linear = this->pImpl->model.JointByName(manager, "cartpole_xacro::linear");
+
+    // Check that all link exist
+    if (pole == ignition::gazebo::kNullEntity) {
+        ignerr << "Failed to find 'pole' link" << std::endl;
         return;
+    }
+    if (cart == ignition::gazebo::kNullEntity) {
+        ignerr << "Failed to find 'cart' link" << std::endl;
+        return;
+    }
+    if (rail == ignition::gazebo::kNullEntity) {
+        ignerr << "Failed to find 'rail' link" << std::endl;
+        return;
+    }
+
+    // Check that all joint exist
+    if (pivot == ignition::gazebo::kNullEntity) {
+        ignerr << "Failed to find 'pivot' joint" << std::endl;
+        return;
+    }
+    if (linear == ignition::gazebo::kNullEntity) {
+        ignerr << "Failed to find 'linear' joint" << std::endl;
+        return;
+    }
+
+    // Generate a random initial angle
+    auto theta0 = pImpl->getRandomTheta();
+
+    // Get the joint position component
+    auto poleJoint = this->pImpl->model.JointByName(manager, "cartpole_xacro::pivot");
+    auto polePos = manager.Component<JointPosition>(poleJoint);
+
+    // Set the random initial angle of the pole
+    if (!polePos) {
+        manager.CreateComponent(poleJoint, JointPosition(theta0));
+    }
+    else {
+        *polePos = JointPosition(theta0);
     }
 }
 
-using namespace ignition::gazebo;
 void CartPole::PreUpdate(const ignition::gazebo::UpdateInfo& /*info*/,
                          ignition::gazebo::EntityComponentManager& manager)
 {
-    auto testLink = [&](const ignition::gazebo::EntityId&,
-                        const components::Link* /*_link*/,
-                        const components::Name* _name,
-                        const components::Pose * /*_pose*/) -> bool {
-        std::cout << _name->Data() << std::endl << std::flush;
-        return true;
-    };
-    manager.Each<ignition::gazebo::components::Link,
-                 ignition::gazebo::components::Name,
-                 ignition::gazebo::components::Pose>(testLink);
-
-    auto testJoint = [&](const ignition::gazebo::EntityId&,
-                         const components::Joint* /*_joint*/,
-                         const components::Name* _name,
-                         const components::Pose * /*_pose*/) -> bool {
-        std::cout << _name->Data() << std::endl << std::flush;
-        return true;
-    };
-    manager.Each<ignition::gazebo::components::Joint,
-                 ignition::gazebo::components::Name,
-                 ignition::gazebo::components::Pose>(testJoint);
-
+    // TODO: the Configure method is called only if this plugin is added
+    //       in the sdf file. Right now gym-ignition loads the plugin internally.
     if (pImpl->firstRun) {
-        auto id = manager.EntityByComponents(ignition::gazebo::components::Model(),
-                                             ignition::gazebo::components::Name("cartpole_xacro"));
-        this->pImpl->model = ignition::gazebo::Model(id);
-
-        auto link = this->pImpl->model.LinkByName(manager, "cartpole_xacro::pole");
-
-        // Fail to create component if link is not found
-        if (link == ignition::gazebo::kNullEntity) {
-            ignerr << "Failed to find link" << std::endl;
-            return;
-        }
-
-        auto joint = this->pImpl->model.JointByName(manager, "cartpole_xacro::pivot");
-
-        // Fail to create component if link is not found
-        if (joint == ignition::gazebo::kNullEntity) {
-            ignerr << "Failed to find joint" << std::endl;
-            return;
-        }
-
+        ignition::gazebo::EventManager eventManager;
+        Configure(0, nullptr, manager, eventManager);
         pImpl->firstRun = false;
     }
 
-    //    if (this->preUpdateCallback) {
-    //        this->preUpdateCallback(info, manager);
-    //    }
-    igndbg << "PreUpdate!" << std::endl;
+    // Get the cart joint position
+    auto cartJoint = this->pImpl->model.JointByName(manager, "cartpole_xacro::linear");
+    auto cartPos = manager.Component<JointPosition>(cartJoint);
+
+    if (!cartPos) {
+        igndbg << "Cart position not yet initialized. Skipping PreUpdate." << std::endl;
+        return;
+    }
+
+    // Set the action
+    // TODO
+    switch (pImpl->action) {
+        case MOVE_LEFT:
+            *cartPos = JointPosition(cartPos->Data() + 0.02);
+            break;
+        case MOVE_RIGHT:
+            *cartPos = JointPosition(cartPos->Data() - 0.02);
+            break;
+        case DONT_MOVE:
+            break;
+    }
 }
 
-// void CartPole::Update(const ignition::gazebo::UpdateInfo& info,
-//                        ignition::gazebo::EntityComponentManager& manager)
-//{
-//    if (this->updateCallback) {
-//        this->updateCallback(info, manager);
-//    }
-//}
-
 void CartPole::PostUpdate(const ignition::gazebo::UpdateInfo& /*info*/,
-                          const ignition::gazebo::EntityComponentManager& /*manager*/)
+                          const ignition::gazebo::EntityComponentManager& manager)
 {
-    //    if (this->postUpdateCallback) {
-    //        this->postUpdateCallback(info, manager);
-    //    }
+    auto cartJoint = this->pImpl->model.JointByName(manager, "cartpole_xacro::linear");
+    auto cartPos = manager.Component<JointPosition>(cartJoint);
+    // auto cartVel = manager.Component<components::JointVelocity>(cartJoint);
+
+    auto poleJoint = this->pImpl->model.JointByName(manager, "cartpole_xacro::pivot");
+    auto polePos = manager.Component<JointPosition>(poleJoint);
+    // auto poleVel = manager.Component<components::JointVelocity>(poleJoint);
+
+    if (!cartPos) {
+        igndbg << "Cart position not yet initialized. Skipping PostUpdate." << std::endl;
+        return;
+    }
+
+    if (!polePos) {
+        igndbg << "Pose position not yet initialized. Skipping PostUpdate." << std::endl;
+        return;
+    }
+
+    {
+        std::lock_guard lock(pImpl->mutex);
+        pImpl->observationBuffer[0] = (180.0 / M_PI) * polePos->Data();
+        pImpl->observationBuffer[1] = cartPos->Data();
+    }
 }
 
 bool CartPole::isDone()
 {
     std::lock_guard lock(pImpl->mutex);
+
+    if (std::abs(pImpl->observationBuffer[0]) > 60.0
+        || std::abs(pImpl->observationBuffer[1]) > 0.98) {
+        return true;
+    }
+
+    return false;
+}
+
+bool CartPole::reset()
+{
+    assert(pImpl->manager);
+
+    // Reset the pole position
+    auto poleJoint = this->pImpl->model.JointByName(*pImpl->manager, "cartpole_xacro::pivot");
+    auto polePos = pImpl->manager->Component<JointPosition>(poleJoint);
+    assert(polePos);
+    auto theta0 = pImpl->getRandomTheta();
+    *polePos = JointPosition(theta0);
+
+    // Reset the cart position
+    auto cartJoint = this->pImpl->model.JointByName(*pImpl->manager, "cartpole_xacro::linear");
+    auto cartPos = pImpl->manager->Component<JointPosition>(cartJoint);
+    assert(cartPos);
+    *cartPos = JointPosition(0);
+
+    {
+        // TODO: store the entities and access them direcly in getObservation?
+        std::lock_guard lock(pImpl->mutex);
+        pImpl->observationBuffer[0] = (180.0 / M_PI) * polePos->Data();
+        pImpl->observationBuffer[1] = cartPos->Data();
+    }
+
     return true;
 }
 
-bool CartPole::setAction(const gympp::gyms::EnvironmentBehavior::Action& /*action*/)
+bool CartPole::setAction(const gympp::gyms::EnvironmentBehavior::Action& action)
 {
     std::lock_guard lock(pImpl->mutex);
+    assert(action.get<size_t>());
+    auto actionValue = (*action.get<size_t>())[0];
+
+    if (actionValue == 0) {
+        pImpl->action = MOVE_LEFT;
+    }
+    else if (actionValue == 1) {
+        pImpl->action = MOVE_RIGHT;
+    }
+    else if (actionValue == 2) {
+        pImpl->action = DONT_MOVE;
+    }
+    else {
+        return false;
+    }
+
     return true;
 }
 
 std::optional<gympp::gyms::EnvironmentBehavior::Reward> CartPole::computeReward()
 {
     std::lock_guard lock(pImpl->mutex);
-    return {};
+    return 1.0; // TODO
 }
 
 std::optional<gympp::gyms::EnvironmentBehavior::Observation> CartPole::getObservation()
 {
     std::lock_guard lock(pImpl->mutex);
-    return {};
+    return Observation(pImpl->observationBuffer);
 }
 
 IGNITION_ADD_PLUGIN(gympp::plugins::CartPole,
--- envs/cpp/CartPole/CartPolePlugin.h
+++ envs/cpp/CartPole/CartPolePlugin.h
@@ -15,16 +15,12 @@ namespace gympp {
     //    } // namespace ignition
 } // namespace gympp
 
-// using TypedEnvironmentBehavior = gympp::gyms::EnvironmentBehavior<size_t, double>;
-
 class gympp::plugins::CartPole final
     : public ignition::gazebo::System
     , public ignition::gazebo::ISystemConfigure
     , public ignition::gazebo::ISystemPreUpdate
     , public ignition::gazebo::ISystemPostUpdate
     , public gympp::gyms::EnvironmentBehavior
-//    , public ::ignition::gazebo::ISystemUpdate
-//    , public TypedEnvironmentBehavior
 {
 private:
     class Impl;
@@ -34,7 +30,7 @@ public:
     CartPole();
     ~CartPole() override = default;
 
-    void Configure(const ignition::gazebo::EntityId& _id,
+    void Configure(const ignition::gazebo::Entity& _id,
                    const std::shared_ptr<const sdf::Element>& _sdf,
                    ignition::gazebo::EntityComponentManager& _ecm,
                    ignition::gazebo::EventManager& _eventMgr) override;
@@ -42,23 +38,14 @@ public:
     void PreUpdate(const ignition::gazebo::UpdateInfo& info,
                    ignition::gazebo::EntityComponentManager& manager) override;
 
-    //    void Update(const ::ignition::gazebo::UpdateInfo& info,
-    //                ::ignition::gazebo::EntityComponentManager& manager) override;
-
     void PostUpdate(const ignition::gazebo::UpdateInfo& info,
                     const ignition::gazebo::EntityComponentManager& manager) override;
 
     bool isDone() override;
+    bool reset() override;
     bool setAction(const Action& action) override;
     std::optional<Reward> computeReward() override;
     std::optional<Observation> getObservation() override;
-
-    // using CallbackType = std::function<void(const ::ignition::gazebo::UpdateInfo&,
-    //                                         const ::ignition::gazebo::EntityComponentManager&)>;
-
-    //    CallbackType preUpdateCallback;
-    //    CallbackType updateCallback;
-    //    CallbackType postUpdateCallback;
 };
 
 #endif // GYMPP_PLUGINS_CARTPOLE
