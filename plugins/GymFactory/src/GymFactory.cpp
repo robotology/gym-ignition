79ef7632f5c2b4abbe04aa0fa72eb03829544519
Diego Ferigo <diego.ferigo@iit.it>
2019-04-15 12:09:24 +0200

    Fixed methods call order in GymFactory
    Now setupGazeboWorld has to ben called before setupIgnitionPlugin

--- plugins/GymFactory/src/GymFactory.cpp
+++ plugins/GymFactory/src/GymFactory.cpp
@@ -76,16 +76,15 @@ gympp::EnvironmentPtr gympp::GymFactory::make(const std::__cxx11::string& envNam
                                                                 observationSpace,
                                                                 /*updateRate=*/50,
                                                                 /*iterations=*/10);
+    // Setup the world
+    if (!ignGym->setupGazeboWorld(md.worldFileName, md.modelNames)) {
+        gymppError << "Failed to setup SDF file";
+        return nullptr;
+    }
 
     // Setup the CartPolePlugin
     if (!ignGym->setupIgnitionPlugin(md.libraryName, md.className)) {
         gymppError << "Failed to setup the ignition plugin" << std::endl;
-        return nullptr;
-    }
-
-    // Setup the world
-    if (!ignGym->setupGazeboWorld(md.worldFileName, md.modelNames)) {
-        gymppError << "Failed to setup SDF file";
         return nullptr;
     }
 
