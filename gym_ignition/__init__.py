3b276630de38ca2c9d5013dd9cbe314fcb43c1be
Diego Ferigo <diego.ferigo@iit.it>
2019-04-18 16:20:16 +0200

    Removed unused lines to initialize verbosity of openai gym

--- gym_ignition/__init__.py
+++ gym_ignition/__init__.py
@@ -3,11 +3,7 @@
 # GNU Lesser General Public License v2.1 or any later version.
 
 from gym.envs.registration import register
-from gym import logger
 from gym_ignition.envs.base.ignition_env import IgnitionEnv
-
-# Set gym verbosity
-logger.set_level(logger.ERROR)
 
 register(
     id='CartPoleIgnition-v0',
