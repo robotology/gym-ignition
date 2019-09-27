7aaccbbf56e66750d719b0f4057d0f47ce454f25
Diego Ferigo <diego.ferigo@iit.it>
2019-09-27 21:14:33 +0200

    Add custom warnings formatting

--- gym_ignition/utils/logger.py
+++ gym_ignition/utils/logger.py
@@ -1,11 +1,47 @@
+# Copyright (C) 2019 Istituto Italiano di Tecnologia (IIT). All rights reserved.
+# This software may be modified and distributed under the terms of the
+# GNU Lesser General Public License v2.1 or any later version.
+
+import warnings
 from gym import logger
-from gym.logger import debug, info, warn, error
-from gym_ignition import gympp_bindings as bindings
+from gym.utils import colorize
+from gym.logger import debug, info, error
+
+try:
+    from gym_ignition import gympp_bindings as bindings
+    bindings_found = True
+except:
+    bindings_found = False
 
 
-def set_level(level):
+def custom_formatwarning(msg, *args, **kwargs):
+    print(args)
+    print(kwargs)
+    # raise
+    if logger.MIN_LEVEL is logger.DEBUG:
+        warning = "{}:{} {}: {}\n".format(args[1], args[2], args[0].__name__, msg)
+    else:
+        warning = "{}\n".format(msg)
+
+    return warning
+
+
+def warn(msg: str, *args) -> None:
+    global MIN_LEVEL
+    if logger.MIN_LEVEL <= logger.WARN:
+        warnings.warn(colorize('%s: %s'%('WARN', msg % args), 'yellow'), stacklevel=2)
+
+
+# Monkey patch warning formatting
+warnings.formatwarning = custom_formatwarning
+
+
+def set_level(level: int) -> None:
     # Set the gym verbosity
     logger.set_level(level)
+
+    if not bindings_found:
+        return
 
     # Set the gympp verbosity
     if logger.MIN_LEVEL <= logger.DEBUG:
