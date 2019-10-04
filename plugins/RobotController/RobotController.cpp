16f6dca1949ed4dc7435c81f6d1b0614744db7e7
Diego Ferigo <diego.ferigo@iit.it>
2019-10-04 15:54:01 +0200

    Update RobotController
    Now this plugin just steps the PID controllers, if enabled

--- plugins/RobotController/RobotController.cpp
+++ plugins/RobotController/RobotController.cpp
@@ -9,9 +9,11 @@
 #include "RobotController.h"
 #include "gympp/Log.h"
 #include "gympp/Robot.h"
-#include "gympp/gazebo/EnvironmentCallbacksSingleton.h"
-#include "gympp/gazebo/IgnitionRobot.h"
+#include "gympp/gazebo/RobotSingleton.h"
 
+#include <ignition/gazebo/Model.hh>
+#include <ignition/gazebo/components/Model.hh>
+#include <ignition/gazebo/components/Name.hh>
 #include <ignition/plugin/Register.hh>
 
 #include <cassert>
@@ -22,14 +24,42 @@
 using namespace gympp::gazebo;
 using namespace gympp::plugins;
 
-using ObservationDataType = int;
-using ObservationSample = gympp::BufferContainer<ObservationDataType>::type;
+// ====
+// IMPL
+// ====
 
 class RobotController::Impl
 {
 public:
+    std::string modelName;
+    bool warningReported = false;
     std::shared_ptr<gympp::Robot> robot = nullptr;
+    static gympp::RobotPtr getRobotPtr(const std::string& robotName);
 };
+
+gympp::RobotPtr RobotController::Impl::getRobotPtr(const std::string& robotName)
+{
+    // Get the robot interface
+    auto robotPtr = RobotSingleton::get().getRobot(robotName).lock();
+
+    // Check that is not a nullptr
+    if (!robotPtr) {
+        gymppWarning << "Failed to get the robot '" << robotName << "' from the singleton"
+                     << std::endl;
+        return {};
+    }
+
+    if (!robotPtr->valid()) {
+        gymppError << "The robot interface is not valid" << std::endl;
+        return {};
+    }
+
+    return robotPtr;
+}
+
+// ===============
+// ROBOTCONTROLLER
+// ===============
 
 RobotController::RobotController()
     : System()
@@ -38,49 +68,71 @@ RobotController::RobotController()
 
 RobotController::~RobotController()
 {
-    auto* ecSingleton = EnvironmentCallbacksSingleton::Instance();
-
-    if (bool removed = ecSingleton->deleteEnvironmentCallback(pImpl->robot->name()); !removed) {
-        gymppError << "Failed to unregister the environment callbacks";
-        assert(removed);
-    }
-}
+    gymppDebug << "Destroying the RobotController of the robot '" << pImpl->modelName << "'"
+               << std::endl;
+};
 
 void RobotController::Configure(const ignition::gazebo::Entity& entity,
-                                const std::shared_ptr<const sdf::Element>& sdf,
+                                const std::shared_ptr<const sdf::Element>& /*sdf*/,
                                 ignition::gazebo::EntityComponentManager& ecm,
                                 ignition::gazebo::EventManager& /*eventMgr*/)
 {
-    // Create an IgnitionRobot object from the ecm
-    auto ignRobot = std::make_shared<gympp::gazebo::IgnitionRobot>();
-    if (!ignRobot->configureECM(entity, sdf, ecm)) {
-        gymppError << "Failed to configure the Robot interface" << std::endl;
+    // Create a model and check its validity
+    auto model = ignition::gazebo::Model(entity);
+    if (!model.Valid(ecm)) {
+        gymppError << "The entity of the parent model of the gympp plugin is not valid"
+                   << std::endl;
+        assert(false);
         return;
     }
 
-    if (!ignRobot->valid()) {
-        gymppError << "The Robot interface is not valid" << std::endl;
-        return;
-    }
-
-    // Store a pointer to gympp::Robot
-    pImpl->robot = ignRobot;
-
-    // Auto-register the environment callbacks
-    gymppDebug << "Registering environment callbacks for robot '" << ignRobot->name() << "'"
-               << std::endl;
-    auto ecSingleton = EnvironmentCallbacksSingleton::Instance();
-    bool registered = ecSingleton->storeEnvironmentCallback(ignRobot->name(), this);
-    assert(registered);
+    // Get the model name and ask the robot interface
+    pImpl->modelName = model.Name(ecm);
 }
 
 void RobotController::PreUpdate(const ignition::gazebo::UpdateInfo& info,
-                                ignition::gazebo::EntityComponentManager& /*ecm*/)
+                                ignition::gazebo::EntityComponentManager& ecm)
 {
     if (info.paused) {
         return;
     }
 
+    // TODO: after model deletion, plugins are not unloaded from the simulation. This means that the
+    //       RobotController keeps running also if its parent model does not exist anymore.
+    //       We check here the existing of the model in the ECM before proceeding.
+    auto modelEntity = ecm.EntityByComponents(ignition::gazebo::components::Model(),
+                                              ignition::gazebo::components::Name(pImpl->modelName));
+    if (modelEntity == ignition::gazebo::kNullEntity) {
+        if (!pImpl->warningReported) {
+            pImpl->warningReported = true;
+            gymppWarning << "The model " << pImpl->modelName
+                         << " does not exist anymore. Its RobotController will be no-op."
+                         << std::endl;
+        }
+        return;
+    }
+
+    // TODO: The model entity removal is a -request- of removal. Depending how it is called, the
+    //       simulator can still finish its simulation step with the entity in pending removal
+    //       state. Currently the ECM has a method IsMarkedForRemoval but it is private.
+
+    // During the process of model creation, this plugin is loaded right after the creation of
+    // all the entities related to links, joints, etc, and before storing the robot interface in
+    // the singleton. This means that asking for the robot interface during the Configure step
+    // is still early. We do it lazily here at the first PreUpdate call.
+    if (!pImpl->robot) {
+        pImpl->robot = pImpl->getRobotPtr(pImpl->modelName);
+
+        if (!pImpl->robot) {
+            gymppWarning << "Failed to get the robot interface. Maybe the plugin is going to "
+                            "be unloaded?"
+                         << std::endl;
+            return;
+        }
+        assert(pImpl->robot);
+    }
+
+    // Update the PID controllers of the robot
     if (!pImpl->robot->update(info.simTime)) {
         assert(false);
         gymppError << "Failed to update the robot controller" << std::endl;
@@ -88,33 +140,7 @@ void RobotController::PreUpdate(const ignition::gazebo::UpdateInfo& info,
     }
 }
 
-bool RobotController::isDone()
-{
-    return false;
-}
-
-bool RobotController::reset()
-{
-    return true;
-}
-
-bool RobotController::setAction(const EnvironmentCallbacks::Action& /*action*/)
-{
-    return true;
-}
-
-std::optional<gympp::gazebo::EnvironmentCallbacks::Reward> RobotController::computeReward()
-{
-    return EnvironmentCallbacks::Reward(0);
-}
-
-std::optional<gympp::gazebo::EnvironmentCallbacks::Observation> RobotController::getObservation()
-{
-    return ObservationSample{0};
-}
-
 IGNITION_ADD_PLUGIN(gympp::plugins::RobotController,
                     gympp::plugins::RobotController::System,
                     gympp::plugins::RobotController::ISystemConfigure,
-                    gympp::plugins::RobotController::ISystemPreUpdate,
-                    gympp::gazebo::EnvironmentCallbacks)
+                    gympp::plugins::RobotController::ISystemPreUpdate)
