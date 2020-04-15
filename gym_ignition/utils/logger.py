2663f1934e074b0bae8651bc7130ac0a109420da
Diego Ferigo <diego.ferigo@iit.it>
2020-04-15 14:40:40 +0200

    Update utils.logger

--- gym_ignition/utils/logger.py
+++ gym_ignition/utils/logger.py
@@ -8,13 +8,17 @@ from gym.utils import colorize
 from gym.logger import debug, info, error
 
 try:
-    from gym_ignition import gympp_bindings as bindings
+    from gym_ignition import scenario_bindings as bindings
     bindings_found = True
-except:
+except ImportError:
     bindings_found = False
 
 
 def custom_formatwarning(msg, *args, **kwargs):
+    """
+    Custom format that overrides :py:func:`warnings.formatwarning`.
+    """
+
     if logger.MIN_LEVEL is logger.DEBUG:
         warning = "{}:{} {}: {}\n".format(args[1], args[2], args[0].__name__, msg)
     else:
@@ -24,6 +28,10 @@ def custom_formatwarning(msg, *args, **kwargs):
 
 
 def warn(msg: str, *args) -> None:
+    """
+    Custom definition of :py:func:`gym.logger.warn` function.
+    """
+
     if logger.MIN_LEVEL <= logger.WARN:
         warnings.warn(colorize('%s: %s' % ('WARN', msg % args), 'yellow'), stacklevel=2)
 
@@ -33,6 +41,21 @@ warnings.formatwarning = custom_formatwarning
 
 
 def set_level(level: int) -> None:
+    """
+    Set the verbosity level of both :py:mod:`gym` and :py:mod:`gym_ignition`.
+
+    Accepted values:
+
+    - :py:const:`gym.logger.DEBUG` (10)
+    - :py:const:`gym.logger.INFO` (20)
+    - :py:const:`gym.logger.WARN` (30)
+    - :py:const:`gym.logger.ERROR` (40)
+    - :py:const:`gym.logger.DISABLED` (50)
+
+    Args:
+        level: The desired verbosity level.
+    """
+
     # Set the gym verbosity
     logger.set_level(level)
 
@@ -41,12 +64,12 @@ def set_level(level: int) -> None:
 
     # Set the gympp verbosity
     if logger.MIN_LEVEL <= logger.DEBUG:
-        bindings.GazeboSimulator.setVerbosity(4)
+        bindings.setVerbosity(4)
     elif logger.MIN_LEVEL <= logger.INFO:
-        bindings.GazeboSimulator.setVerbosity(3)
+        bindings.setVerbosity(3)
     elif logger.MIN_LEVEL <= logger.WARN:
-        bindings.GazeboSimulator.setVerbosity(2)
+        bindings.setVerbosity(2)
     elif logger.MIN_LEVEL <= logger.ERROR:
-        bindings.GazeboSimulator.setVerbosity(1)
+        bindings.setVerbosity(1)
     else:
         raise Exception("Verbosity level not recognized")
