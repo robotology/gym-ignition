6062f12b7b9b192ff3852e2c4e94661f6a69d176
Diego Ferigo <diego.ferigo@iit.it>
2020-04-15 17:14:40 +0200

    Setup the environment variables for gym-ignition-models

--- gym_ignition/__init__.py
+++ gym_ignition/__init__.py
@@ -2,7 +2,10 @@
 # This software may be modified and distributed under the terms of the
 # GNU Lesser General Public License v2.1 or any later version.
 
-import os
+# Workaround for https://github.com/osrf/sdformat/issues/227.
+# It has to be done before loading the bindings.
+import gym_ignition_models
+gym_ignition_models.setup_environment()
 
 # Import SWIG bindings
 # See https://github.com/robotology/gym-ignition/issues/7
@@ -44,5 +47,6 @@ from gym_ignition.utils import gazebo_env_vars, resource_finder
 gazebo_env_vars.setup_gazebo_env_vars()
 
 # Add IGN_GAZEBO_RESOURCE_PATH to the default search path
+import os
 if "IGN_GAZEBO_RESOURCE_PATH" in os.environ:
     resource_finder.add_path_from_env_var("IGN_GAZEBO_RESOURCE_PATH")
