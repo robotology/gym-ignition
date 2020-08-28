a12d5544a6e64edccb076f0ddbdb45fb7e9ca718
Anqing Duan <anqing.duan@iit.it>
2020-08-28 15:56:28 +0200

    Minor fix

--- examples/scenario/setPandaState.py
+++ examples/scenario/setPandaState.py
@@ -3,14 +3,16 @@
 # GNU Lesser General Public License v2.1 or any later version.
 
 from gym_ignition_environments import models
-from gym_ignition import scenario_bindings as scenario
+# from gym_ignition import scenario_bindings as scenario
+from scenario import gazebo as scenario_gazebo
+from scenario import core as scenario_core
 from gym_ignition.utils.scenario import init_gazebo_sim
 import gym_ignition_models
 import time
 from typing import List
 
 # Set the verbosity
-scenario.set_verbosity(level=1)
+scenario_gazebo.set_verbosity(scenario_gazebo.Verbosity_debug)
 
 # Get the default simulator and the default empty world
 gazebo, world = init_gazebo_sim()
@@ -20,40 +22,39 @@ gazebo.gui()
 # gazebo.run(paused=True)
 time.sleep(3)
 
-
 # Create a Panda class that automatically inserts the model in the world
-class Panda(scenario.Model):
+class Panda(scenario_core.Model):
 
     def __init__(self,
-                 world: scenario.World,
+                 world: scenario_core.World,
                  position: List[float] = (0., 0, 0),
                  orientation: List[float] = (1., 0, 0, 0)):
         # Initialize the base class
-        super().__init__()
+        # super().__init__()
 
         # Get the model file
         urdf = gym_ignition_models.get_model_file("panda")
 
         # Insert the model in the world
         name = "panda_manipulator"
-        pose = scenario.Pose(position, orientation)
-        world.insert_model(urdf, pose, name)
+        pose = scenario_core.Pose(position, orientation)
+        world.to_gazebo().insert_model(urdf, pose, name)
 
         # Get and store the model from the world
-        self.model = world.get_model(model_name=name)
+        self.model = world.get_model(model_name=name).to_gazebo()
 
-    # def __getattr__(self, name):
-    #     print("getting", name)
-    #     return getattr(self.model, name)
+    def __getattr__(self, name):
+        # print("getting", name)
+        return getattr(self.model, name)
 
 
 panda_pos = [0, 0, 0]
 panda_ori = [1, 0, 0, 0]  # quaternion: w,x,y,z
 panda = Panda(world=world, position=panda_pos, orientation=panda_ori)
 
-# des_jnt_pos = [0.0] * panda.model.dofs()
-# panda.model.reset_joint_positions(des_jnt_pos)
-# assert panda.model.reset_joint_positions(des_jnt_pos)
+des_jnt_pos = [0.0] * panda.dofs()
+panda.reset_joint_positions(des_jnt_pos)
+assert panda.reset_joint_positions(des_jnt_pos)
 
 # step a few seconds of simulation
 for i in range(3000):
