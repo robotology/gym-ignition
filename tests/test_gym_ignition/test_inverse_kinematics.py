f4d733a9ccd1e162933d3330dea58f3fe8df4798
Diego Ferigo <diego.ferigo@iit.it>
2020-11-09 14:42:48 +0100

    Update old IK test to Scenario

--- tests/test_gym_ignition/test_inverse_kinematics.py
+++ tests/test_gym_ignition/test_inverse_kinematics.py
@@ -2,62 +2,59 @@
 # This software may be modified and distributed under the terms of the
 # GNU Lesser General Public License v2.1 or any later version.
 
+import pytest
+pytestmark = pytest.mark.gym_ignition
+
 import numpy as np
-from . import utils
-from gym_ignition.robots import sim
-import gym_ignition_models as models
-from gym_ignition.utils.inverse_kinematics_nlp import TargetType
-from gym_ignition.base.robot.robot_joints import JointControlMode, PID
-from gym_ignition.utils.inverse_kinematics_nlp import InverseKinematicsNLP
+from typing import Tuple
+from gym_ignition_environments import models
+from scenario import gazebo as scenario_gazebo
+from gym_ignition.context.gazebo import controllers
+from gym_ignition.rbd.idyntree import inverse_kinematics_nlp
+from ..common.utils import default_world_fixture as default_world
 
-# From: https://github.com/mkrizmancic/franka_gazebo/blob/master/config/default.yaml
-# These gains work well with a physics step of 1000 Hz.
-pid_gains_1000_hz = {
-    'panda_joint1': PID(p=50, i=0, d=20),
-    'panda_joint2': PID(p=10000, i=0, d=500),
-    'panda_joint3': PID(p=100, i=0, d=10),
-    'panda_joint4': PID(p=1000, i=0, d=50),
-    'panda_joint5': PID(p=100, i=0, d=10),
-    'panda_joint6': PID(p=100, i=0, d=10),
-    'panda_joint7': PID(p=10, i=0.5, d=0.1),
-    'panda_finger_joint1': PID(p=100, i=0, d=50),
-    'panda_finger_joint2': PID(p=100, i=0, d=50),
-}
+# Set the verbosity
+scenario_gazebo.set_verbosity(scenario_gazebo.Verbosity_debug)
 
 
-def test_inverse_kinematics():
-    # Get the simulator
-    physics_rate = 1000.0
-    gazebo = utils.Gazebo(physics_rate=physics_rate)
+@pytest.mark.parametrize("default_world", [(1.0 / 1_000, 1.0, 1)], indirect=True)
+def test_inverse_kinematics(default_world: Tuple[scenario_gazebo.GazeboSimulator,
+                                              scenario_gazebo.World]):
+
+    # Get the simulator and the world
+    gazebo, world = default_world
 
     # Get the robot
-    panda = sim.gazebo.panda.PandaRobot(gazebo=gazebo.simulator)
+    panda = models.panda.Panda(world=world)
+    panda.enable_self_collisions(enable=False)
+    gazebo.run(paused=True)
 
-    # Set the PID gains
-    for joint_name, pid in pid_gains_1000_hz.items():
-        ok_pid = panda.set_joint_pid(joint_name=joint_name, pid=pid)
-        assert ok_pid
+    # Control all the joints
+    controlled_joints = panda.joint_names()
 
-    # Configure the PID controllers step time
-    ok_dt = panda.set_dt(step_size=(1.0 / physics_rate))
-    assert ok_dt
+    # Set the controller period
+    assert panda.set_controller_period(gazebo.step_size())
+    assert panda.controller_period() == gazebo.step_size()
 
-    # Get the controlled joints (no fingers)
-    controlled_joints = [j for j in panda.joint_names() if "_finger_" not in j]
-
-    # Control the robot in position
-    for joint_name in panda.joint_names():
-        ok_mode = panda.set_joint_control_mode(joint_name, JointControlMode.POSITION)
-        assert ok_mode, f"Failed to control joint {joint_name} in position"
+    # Insert the ComputedTorqueFixedBase controller
+    assert panda.to_gazebo().insert_model_plugin(*controllers.ComputedTorqueFixedBase(
+        kp=[20.0] * panda.dofs(),
+        ki=[0.0] * panda.dofs(),
+        kd=[5.0] * panda.dofs(),
+        urdf=panda.get_model_file(),
+        joints=controlled_joints,
+    ).args())
 
     # Create the IK object
-    ik = InverseKinematicsNLP(urdf_filename=models.get_model_file("panda"),
-                              considered_joints=controlled_joints)
+    ik = inverse_kinematics_nlp.InverseKinematicsNLP(
+        urdf_filename=panda.get_model_file(),
+        considered_joints=controlled_joints,
+        joint_serialization=panda.joint_names())
 
     # Initialize IK
     ik.initialize(verbosity=1,
-                  cost_tolerance=1e-10,
-                  floating_base=False)
+                  floating_base=False,
+                  cost_tolerance=1E-8)
 
     # There should be no active targets
     assert len(ik.get_active_target_names()) == 0
@@ -65,41 +62,63 @@ def test_inverse_kinematics():
     # Add the cartesian target of the end effector
     end_effector = "end_effector_frame"
     ik.add_target(frame_name=end_effector,
-                  target_type=TargetType.POSE)
+                  target_type=inverse_kinematics_nlp.TargetType.POSE,
+                  as_constraint=False)
 
     assert set(ik.get_active_target_names()) == {end_effector}
-    assert ik.get_target_data(target_name=end_effector).type == TargetType.POSE
+    assert ik.get_target_data(target_name=end_effector).type == \
+           inverse_kinematics_nlp.TargetType.POSE
 
-    target_ee_position = np.array([0.3, 0, 0.8])
-    target_ee_quaternion = np.array([0.0, 0.0, 0.0, 1.0])
+    # Desired EE position
+    target_ee_position = np.array([0.5, -0.3, 1.0])
+    target_ee_quaternion = np.array([0.0, 1.0, 0.0, 0.0])
 
-    # Set the target transform
+    # Update the target transform
     ik.update_transform_target(target_name=end_effector,
                                position=target_ee_position,
                                quaternion=target_ee_quaternion)
 
+    # Check that the target transform was stored
+    assert isinstance(ik.get_target_data(target_name=end_effector).data,
+                      inverse_kinematics_nlp.TransformTargetData)
+    assert ik.get_target_data(target_name=end_effector).data.position == \
+           pytest.approx(target_ee_position)
+    assert ik.get_target_data(target_name=end_effector).data.quaternion == \
+           pytest.approx(target_ee_quaternion)
+
+    # Set the current configuration
+    ik.set_current_robot_configuration(
+        base_position=np.array(panda.base_position()),
+        base_quaternion=np.array(panda.base_orientation()),
+        joint_configuration=np.array(panda.joint_positions()))
+
     # Solve the IK problem
     ik.solve()
 
-    # Get the solution
-    ik_solution = ik.get_solution()
+    # Get the full solution
+    ik_solution = ik.get_full_solution()
 
+    # Validate the solution
     assert ik_solution.joint_configuration.size == len(controlled_joints)
-    assert np.allclose(ik_solution.base_position, np.array([0.0, 0, 0]))
-    assert np.allclose(ik_solution.base_quaternion, np.array([1.0, 0, 0, 0]))
+    assert ik_solution.base_position == pytest.approx(np.array([0.0, 0, 0]))
+    assert ik_solution.base_quaternion == pytest.approx(np.array([1.0, 0, 0, 0]))
 
     # Set the desired joint configuration
-    for joint_name, position in zip(controlled_joints, ik_solution.joint_configuration):
-        ok_pos = panda.set_joint_position(joint_name, position)
-        assert ok_pos
+    assert panda.set_joint_position_targets(ik_solution.joint_configuration,
+                                            controlled_joints)
+    assert panda.set_joint_velocity_targets([0.0] * len(controlled_joints))
+    assert panda.set_joint_acceleration_targets([0.0] * len(controlled_joints))
 
-    for _ in range(1000):
-        gazebo.step()
+    for _ in range(5_000):
+        gazebo.run()
+
+    # Check that the desired joint configuration has been reached
+    assert panda.joint_positions(controlled_joints) == \
+           pytest.approx(ik_solution.joint_configuration, abs=np.deg2rad(0.1))
 
     # Get the end effector pose
-    ee_position, ee_quaternion = panda.link_pose(link_name=end_effector)
+    ee_position = panda.get_link(link_name=end_effector).position()
+    ee_quaternion = panda.get_link(link_name=end_effector).orientation()
 
-    assert np.allclose(target_ee_position, ee_position, atol=0.02)
-    assert np.allclose(target_ee_quaternion, ee_quaternion, atol=0.03)
-
-    gazebo.close()
+    assert ee_position == pytest.approx(target_ee_position, abs=0.005)
+    assert ee_quaternion == pytest.approx(target_ee_quaternion, abs=0.005)
