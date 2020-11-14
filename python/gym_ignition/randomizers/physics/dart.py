16c90e3661b54b2601f8a8afdad82b5e49ab20fb
Diego Ferigo <diego.ferigo@iit.it>
2020-11-14 14:59:42 +0100

    Update randomizers
    - Unify input argument to always receive a task object
    - Renamed ModelRandomizer to ModelDescriptionRandomizer
    - New ModelRandomizer that returns a Model object

--- python/gym_ignition/randomizers/physics/dart.py
+++ python/gym_ignition/randomizers/physics/dart.py
@@ -2,14 +2,17 @@
 # This software may be modified and distributed under the terms of the
 # GNU Lesser General Public License v2.1 or any later version.
 
+import gym_ignition.base.task
 from scenario import gazebo as scenario
 from gym_ignition.randomizers.base import physics
 
 
 class DART(physics.PhysicsRandomizer):
     """
-    Class that configures the Ignition Gazebo physics with the DART physics engine and
-    no randomization.
+    Class that configures the Gazebo World of a Task to use the DART physics engine.
+
+    Note:
+        This class does not apply any physics randomization.
     """
 
     def __init__(self, seed: int = None):
@@ -19,10 +22,8 @@ class DART(physics.PhysicsRandomizer):
         if seed is not None:
             self.seed_physics_randomizer(seed=seed)
 
-    def randomize_physics(self, world: scenario.World) -> None:
+    def randomize_physics(self, task: gym_ignition.base.task.Task) -> None:
 
-        # Insert the physics
-        ok_physics = world.set_physics_engine(scenario.PhysicsEngine_dart)
-
-        if not ok_physics:
+        # Set Gazebo to use the DART physics engine
+        if not task.world.to_gazebo().set_physics_engine(scenario.PhysicsEngine_dart):
             raise RuntimeError("Failed to insert the physics plugin")
