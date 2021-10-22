ff70cfea5cfa33035b499d1b2a907f096be569c0
Diego Ferigo <diego.ferigo@iit.it>
2021-10-22 11:08:16 +0200

    Help CMake to find the right Python when called from setup.py

--- scenario/setup.py
+++ scenario/setup.py
@@ -2,6 +2,8 @@
 # This software may be modified and distributed under the terms of the
 # GNU Lesser General Public License v2.1 or any later version.
 
+import sys
+
 import setuptools
 from cmake_build_extension import BuildExtension, CMakeExtension
 
@@ -14,6 +16,7 @@ setuptools.setup(
             cmake_configure_options=[
                 "-DSCENARIO_CALL_FROM_SETUP_PY:BOOL=ON",
                 "-DSCENARIO_BUILD_SHARED_LIBRARY:BOOL=OFF",
+                f"-DPython3_EXECUTABLE:PATH={sys.executable}",
             ],
             cmake_depends_on=["idyntree"],
             disable_editable=True,
